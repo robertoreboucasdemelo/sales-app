13ed0b731004eb2191b73553f4820b6a2324bf39
moduretick <robertoreboucasdemelo@gmail.com>
2021-09-03 14:37:43 -0300

    Project Created

---
+++ backend/src/test/java/com/rrm14/salesapp/SalesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrm14.salesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
