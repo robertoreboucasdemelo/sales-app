edef87760b5ef411291cd10e5008085dd930d464
moduretick <robertoreboucasdemelo@gmail.com>
2021-09-06 15:48:53 -0300

    Add Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,28 @@
 import Footer from "components/footer";
 import NavBar from "components/navbar";
 import DataTable from "components/datatable";
+import BarChart from "components/barcharts";
+import DonutChart from "components/donutcharts";
 
 function App() {
   return (
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Sales App</h1>
+        <h1 className="text-primary py-3">Sales Dashboard</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Success Rate (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">All Sales</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary">All Sales</h2>
+        </div>
         <DataTable />
       </div>
       <Footer />
---
+++ frontend/src/components/donutcharts/index.tsx
@@ -0,0 +1,25 @@
+import Chart from "react-apexcharts";
+
+const mockData = {
+  series: [477138, 499928, 444867, 220426, 473088],
+  labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
+};
+
+const options = {
+  legend: {
+    show: true,
+  },
+};
+
+const DonutChart = () => {
+  return (
+    <Chart
+      options={{ ...options, labels: mockData.labels }}
+      series={mockData.series}
+      type="donut"
+      height="240"
+    />
+  );
+};
+
+export default DonutChart;
